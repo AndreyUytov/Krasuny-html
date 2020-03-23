c6e02f6f3fc10a30136cc4013c5451d6dab91dc0
Андрей <uytov1991@gmail.com>
2020-03-23 20:31:52 +0300

    баг с build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require("webpack");
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const autoprefixer = require("autoprefixer");
